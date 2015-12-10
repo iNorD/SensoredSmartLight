10ad3d219fe3416384c3e224d69903f3eee92cf7
Egor Eremeev <e.eremeev@man-co.ru>
2015-12-10 16:36:15 +0300

    Локализация на английский. Некоторые исправления.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SensoredSmartLight
-README
+Module for z-way home automation system. Turns on light on needed lux level.
--- index.js
+++ index.js
@@ -5,14 +5,17 @@
  -----------------------------------------------------------------------------
  Author: Egor Eremeev
  Description: In the daytime the light turns on some level
-******************************************************************************/
+ ******************************************************************************/
 
 // ----------------------------------------------------------------------------
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
-function SensoredSmartLight (id, controller) {
+function SensoredSmartLight(id, controller) {
     // Call superconstructor first (AutomationModule)
     SensoredSmartLight.super_.call(this, id, controller);
+
+    // by default dimmer button status not pressed
+    this.dimmerButtonStatus = 0;
 
     // Create instance variables
     this.timerAutoOff = null;
@@ -37,16 +40,24 @@ SensoredSmartLight.prototype.init = function (config) {
 
 
     // Morning start
-    var morningStartTime_arr = this.config.Morning.MorningTimeStart.split(":").map(function(x) { return parseInt(x, 10); });
+    var morningStartTime_arr = this.config.Morning.MorningTimeStart.split(":").map(function (x) {
+        return parseInt(x, 10);
+    });
     this.morningStartTime = morningStartTime_arr[0] * 60 + morningStartTime_arr[1];
     // Morning end
-    var morningEndTime_arr = this.config.Morning.MorningTimeEnd.split(":").map(function(x) { return parseInt(x, 10); });
+    var morningEndTime_arr = this.config.Morning.MorningTimeEnd.split(":").map(function (x) {
+        return parseInt(x, 10);
+    });
     this.morningEndTime = morningEndTime_arr[0] * 60 + morningEndTime_arr[1];
     // Day start
-    var dayStartTime_arr = this.config.Day.DayTimeStart.split(":").map(function(x) { return parseInt(x, 10); });
+    var dayStartTime_arr = this.config.Day.DayTimeStart.split(":").map(function (x) {
+        return parseInt(x, 10);
+    });
     this.dayStartTime = dayStartTime_arr[0] * 60 + dayStartTime_arr[1];
     // Day end
-    var dayEndTime_arr = this.config.Day.DayTimeEnd.split(":").map(function(x) { return parseInt(x, 10); });
+    var dayEndTime_arr = this.config.Day.DayTimeEnd.split(":").map(function (x) {
+        return parseInt(x, 10);
+    });
     this.dayEndTime = dayEndTime_arr[0] * 60 + dayEndTime_arr[1];
 
     // handler for Sensor
@@ -61,10 +72,12 @@ SensoredSmartLight.prototype.init = function (config) {
             //Максимум люкс, которые выдает лампочка
             var lightbulbLumens = this.config.RoomParams.LightbulbLumens;
             var roomArea = this.config.RoomParams.RoomArea;
-            var maxBulbLux = lightbulbLumens/roomArea;
+            //var maxBulbLux = lightbulbLumens / roomArea;
+            var maxBulbLux = 40;
+
 
             //Нужный уровень лампочки в процентах
-            var neededLevel;
+            var neededLevel = 0;
 
             //var currentLuminosityLevelLux = self.controller.devices.get(self.config.LuminositySensor).get("metrics:level");
             var currentLuminosityLevelLux = 40;
@@ -72,23 +85,29 @@ SensoredSmartLight.prototype.init = function (config) {
 
 
             //MORNING TIME
-            if (nowTime >= self.morningStartTime && nowTime <= self.morningEndTime) { neededLevelLux = morningLevelLux; }
+            if (nowTime >= self.morningStartTime && nowTime <= self.morningEndTime) {
+                neededLevelLux = morningLevelLux;
+            }
             //DAY TIME
-            else if (nowTime >= self.dayStartTime && nowTime <= self.dayEndTime) { neededLevelLux = dayLevelLux; }
+            else if (nowTime >= self.dayStartTime && nowTime <= self.dayEndTime) {
+                neededLevelLux = dayLevelLux;
+            }
             //NIGHT TIME
-            else { neededLevelLux = nightLevelLux; }
-
-            var diffreneceInLux = neededLevelLux-currentLuminosityLevelLux;
-            if(diffreneceInLux>0) {
-                neededLevel = (diffreneceInLux*100)/maxBulbLux;
+            else {
+                neededLevelLux = nightLevelLux;
             }
 
-            self.controller.devices.get(self.config.Dimmer).performCommand("exact", { level: neededLevel });
+            var diffreneceInLux = neededLevelLux - currentLuminosityLevelLux;
+            if (diffreneceInLux > 0) {
+                neededLevel = (diffreneceInLux * 100) / maxBulbLux;
+            }
+
+            self.controller.devices.get(self.config.Dimmer).performCommand("exact", {level: neededLevel});
             self.controller.devices.get(self.config.Dimmer).performCommand("on");
 
             //AutoOff
             // If timeout setted, start timer autooff
-            if (self.config.timeout !==0 && typeof self.config.timeout !== 'undefined') {
+            if (self.config.timeout !== 0 && typeof self.config.timeout !== 'undefined') {
                 if (self.timerAutoOff) {
                     // Timer is set, so we destroy it
                     clearTimeout(self.timerAutoOff);
@@ -98,26 +117,36 @@ SensoredSmartLight.prototype.init = function (config) {
                     self.controller.devices.get(self.config.Dimmer).performCommand("off");
                     // And clearing out this.timer variable
                     self.timerAutoOff = null;
-                }, self.config.timeout*1000);
+                }, self.config.timeout * 1000);
             }
 
         }
 
     };
 
+    // handler for Dimmer Light
+    this.dimmerLevelChanged = function (Dimmer) {
+        // when light off, dimmer button status not pressed
+        if (self.controller.devices.get(self.config.Dimmer).get("metrics:level") === 0) {
+            self.dimmerButtonStatus = 0;
+        }
+    };
+
 
     // Setup metric update event listener
     this.controller.devices.on(this.config.MotionSensor, 'change:metrics:level', this.sensorTriggered);
+    this.controller.devices.on(this.config.Dimmer, 'change:metrics:level', this.dimmerLevelChanged);
 };
 
 SensoredSmartLight.prototype.stop = function () {
     SensoredSmartLight.super_.prototype.stop.call(this);
 
-    if (this.timerAutoOff){
+    if (this.timerAutoOff) {
         clearTimeout(this.timerAutoOff);
     }
 
     this.controller.devices.off(this.config.MotionSensor, 'change:metrics:level', this.sensorTriggered);
+    this.controller.devices.off(this.config.Dimmer, 'change:metrics:level', this.dimmerLevelChanged);
 
 
 };
